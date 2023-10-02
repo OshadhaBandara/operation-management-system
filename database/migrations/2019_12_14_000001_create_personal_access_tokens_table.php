acea260b83784821d515caaa62785814eb031810
Heshan93 <heshanrajitha1@gmail.com>
2023-10-02 13:15:57 +0530

    Git init

---
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonalAccessTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personal_access_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->string('tokenable_type')->collation('utf8mb4_unicode_ci');
+            $table->unsignedBigInteger('tokenable_id')->collation('utf8mb4_unicode_ci');
+            $table->string('name');
+            $table->string('token', 64)->unique();
+            $table->text('abilities')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamps();
+    
+            // Remove the custom index name
+            $table->index(['tokenable_type', 'tokenable_id']); // Let Laravel generate the index name
+        });
+    }
+    
+    
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personal_access_tokens');
+    }
+}
