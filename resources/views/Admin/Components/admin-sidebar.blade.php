05822a729f3a782047c56d819de919d00c77b091
Heshan93 <heshanrajitha1@gmail.com>
2023-10-23 00:59:36 +0530

    .

--- resources/views/Admin/Components/admin-sidebar.blade.php
+++ resources/views/Admin/Components/admin-sidebar.blade.php
@@ -64,7 +64,7 @@
             <!-- /menu footer buttons -->
             <div class="sidebar-footer hidden-small" >
 
-              <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html" style="width: 100%">
+              <a data-toggle="tooltip" data-placement="top" title="Logout" href="logout_admin" style="width: 100%">
                 <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
               </a>
             </div>
